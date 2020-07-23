c8e662f2be148b0145f3b77a98855457a7f900b1
19021996 <19021996@DESKTOP-KNIU2O2>
2020-07-23 15:50:58 +0800

    added comment

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -177,7 +177,7 @@ public class ResourceCentre {
 		
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
-		// write your code here
+		// write your code here 1
 		//WinnieYong
 		chromebookList.add(cb);
 		System.out.println("Chromebook added");
